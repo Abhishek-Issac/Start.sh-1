d60e89f7667599ea39a349920b1b93f4c3a68dce
Crazyboss1 <112222385+Crazyboss1@users.noreply.github.com>
2023-10-18 06:20:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://Abhishek-issac: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@github.com/Abhishek-issac/TigerShroffv4.git /TigerShroffv3
+  git clone https://Abhishek-issac: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@github.com/Abhishek-issac/TigerShroffv4.git /TigerShroffv4
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TigerShroffv3
+  git clone $UPSTREAM_REPO /TigerShroffv4
 fi
-cd /TigerShroffv3
+cd /TigerShroffv4
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
